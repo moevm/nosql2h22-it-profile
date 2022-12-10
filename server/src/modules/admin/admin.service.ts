8799f2f14145d59902ea1ddab6a497763a65477a
Miha1l <mr.loykonen@mail.ru>
2022-12-11 01:44:13 +0300

    itp-41: add importData

--- server/src/modules/admin/admin.service.ts
+++ server/src/modules/admin/admin.service.ts
@@ -6,4 +6,17 @@ export class AdminService {
 
         return type;
     }
+
+    async importData(body: any) {
+        // let type = body.type;
+        
+        let users = body.users;
+        let experiences = body.experiences;
+        let educations = body.educations;
+        let additionalInformations = body.additions;
+
+        // Здесь запрос к БД на добавление данных в соответствующие коллекции
+
+        return body;
+    }
 }
